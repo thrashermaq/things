3a414d90013cdf77ad8c897ad625d04626cd20d4
Nils Müller <mueller_nils95@yahoo.de>
2019-03-11 16:58:15 +0100

    - Nur eigene Todos laden

--- src/app/todo/service/todo.service.ts
+++ src/app/todo/service/todo.service.ts
@@ -1,19 +1,32 @@
-import {Injectable} from '@angular/core';
-import {AngularFirestore, DocumentReference} from '@angular/fire/firestore';
+import {Injectable, OnInit} from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection, DocumentReference} from '@angular/fire/firestore';
 import {Todo} from '../model/todo.model';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
+import {AuthService} from '../../auth/service/auth.service';
 
 @Injectable()
-export class TodoService {
+export class TodoService implements OnInit {
 
-  private path = 'todos';
+  private todos: AngularFirestoreCollection<Todo>;
 
-  constructor(private firestore: AngularFirestore) {
+  constructor(private firestore: AngularFirestore, private authService: AuthService) {
+    this.authService.user.pipe(
+      map(usr => {
+        if (usr != null) {
+          return this.firestore.collection<Todo>(`users/${usr.uid}/todos`);
+        } else {
+          return null;
+        }
+      })
+    ).subscribe(todos => this.todos = todos);
+  }
+
+  ngOnInit(): void {
   }
 
   getTodos(): Observable<Todo[]> {
-    return this.firestore.collection<Todo>(this.path).snapshotChanges().pipe(
+    return this.todos.snapshotChanges().pipe(
       map(dcaArray => dcaArray.map(dca => {
         const todo = dca.payload.doc.data();
         todo.id = dca.payload.doc.id;
@@ -23,7 +36,7 @@ export class TodoService {
   }
 
   getTodo(id: string): Observable<Todo> {
-    return this.firestore.doc<Todo>(this.path + '/' + id).valueChanges().pipe(
+    return this.todos.doc<Todo>(id).valueChanges().pipe(
       map(todo => {
         todo.id = id;
         return todo;
@@ -32,18 +45,18 @@ export class TodoService {
   }
 
   deleteTodo(id: string): Promise<void> {
-    return this.firestore.doc<Todo>(this.path + '/' + id).delete();
+    return this.todos.doc<Todo>(id).delete();
   }
 
   updateTodo(todo: Todo): Promise<void> {
     const id = todo.id;
     delete todo.id;
-    return this.firestore.doc<Todo>(this.path + '/' + id).update({...todo});
+    return this.todos.doc<Todo>(id).update({...todo});
   }
 
   createTodo(todo: Todo): Promise<DocumentReference> {
     delete todo.id;
-    return this.firestore.collection<Todo>(this.path).add({...todo});
+    return this.todos.add({...todo});
   }
 
 }
